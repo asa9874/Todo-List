fe84c4737041d60507dc265954406535d36971cd
asa9874 <pjkpjk1129@gmail.com>
2025-01-12 11:05:22 +0900

    Feat: Update 로직 완성

--- src/pages/TodoLists/components/Todo/index.tsx
+++ src/pages/TodoLists/components/Todo/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { TODOTask } from "../TodoListContainer"
 import styles from "./Todo.module.css"
 
@@ -8,13 +9,35 @@ interface TodoProps {
 }
 
 function Todo(props: TodoProps) {
+  const [isEditing, setIsEditing] = useState(false); 
+  const [editedName, setEditedName] = useState(props.task.name);  
+
+  function toggleEditMode() {
+    if (isEditing) props.updateTask({ id: props.task.id, name: editedName });
+    setIsEditing(!isEditing);
+  }
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setEditedName(event.target.value);
+  }
+
     return (
       <tr>
         <th><input type="checkbox" className={styles.todocheckbox}/></th>
-        <th>{props.task.name}</th>
+        <th className="taskname">
+            {isEditing ? (
+            <input
+              type="text"
+              value={editedName}
+              onChange={handleInputChange}
+              className={styles.todoInput}
+            />
+          ) : (
+            props.task.name
+          )}
+        </th>
         <th>
           <button className={styles.tododeletebutton} onClick={() => props.deleteTask(props.task.id)}></button>  
-          <button className={styles.todomodifybutton} onClick={() => props.updateTask({id: props.task.id, name: "공부하기!!!!"})}></button>
+          <button className={styles.todomodifybutton} onClick={toggleEditMode}></button>
         </th>
       </tr>
     )
